b17be203589a58f5e60259614a635a433c6ae4fa
xiejunbiao <996790454@qq.com>
2020-12-09 11:29:40 +0800

    [ADD]

--- data/get_data.py
+++ data/get_data.py
@@ -13,7 +13,8 @@ ts.set_token(token)
 
 pro = ts.pro_api()
 
-def get_stock(path, number):
+
+def get_stock(path, number, time_=None):
     # data = pro.query('daily', ts_code='number', start_date='20180102', end_date='20180307')
     # data_test = pro.query('daily', ts_code='number', start_date='20180308', end_date='20180321')
     # data_L = pro.query('daily', ts_code='number', start_date='20180322', end_date='20180404')
@@ -22,27 +23,39 @@ def get_stock(path, number):
     # data_test = pro.daily(ts_code='number', start_date='20180308', end_date='20180321')
     # data_L = pro.daily(ts_code='number', start_date='20180322', end_date='20180404')
     # return ts.get_hist_data(number, start='2018-01-12', end='2018-03-03')
-    data = pro.daily(ts_code=number, start_date='20180102', end_date='20180307')
-    data_test = pro.daily(ts_code=number, start_date='20190308', end_date='20180321')
-    data_L = pro.daily(ts_code=number, start_date='20180322', end_date='20180404')
-    # data = ts.get_hist_data(number, start='2018-01-02', end='2018-03-07')
-    # data_test = ts.get_hist_data(number, start='2019-03-08', end='2018-03-21')
-    # data_L = ts.get_hist_data(number, start='2018-03-22', end='2018-04-04')
-    data.to_csv(path + '/%s.csv' % number)
-    data_test.to_csv(path + '/%s.csv' % (number+'_test'))
-    data_L.to_csv(path + '/%s.csv' % ('L' + number))
+    if time_:
+        data = pro.daily(ts_code=number, start_date=time_['start_date'], end_date=time_['end_date'])
+        data.to_csv(path + '/%s.csv' % number)
+    else:
+        data = pro.daily(ts_code=number, start_date='20180102', end_date='20180307')
+        data_test = pro.daily(ts_code=number, start_date='20190308', end_date='20180321')
+        data_L = pro.daily(ts_code=number, start_date='20180322', end_date='20180404')
+        # data = ts.get_hist_data(number, start='2018-01-02', end='2018-03-07')
+        # data_test = ts.get_hist_data(number, start='2019-03-08', end='2018-03-21')
+        # data_L = ts.get_hist_data(number, start='2018-03-22', end='2018-04-04')
+        data.to_csv(path + '/%s.csv' % number)
+        data_test.to_csv(path + '/%s.csv' % (number+'_test'))
+        data_L.to_csv(path + '/%s.csv' % ('L' + number))
 
 
 def get_concept_classified():
     return ts.get_concept_classified()
 
 
-def get_all_data(number_list, path):
+def get_all_data(number_list, path, start_time=None, end_time=None):
     for i in number_list:
         path_t = path + "/%s" % i
         print(path + "/%s" % i)
         check_dir_exist(path_t)
-        get_stock(path_t, i)
+        if start_time and end_time:
+            time_ = {'start_date': start_time,
+                     'end_time': end_time}
+            get_stock(path_t, i, time_=time_)
+        elif start_time is None and end_time is None:
+            time_=None
+            get_stock(path_t, i, time_=time_)
+        else:
+            print('The time must be either all empty or all non-empty')
 
 
 def check_dir_exist(dir):
@@ -53,7 +66,9 @@ def check_dir_exist(dir):
 
 if __name__ == '__main__':
     path = "E:\\Document\\python\\deep_python\\Optimization_algorithm\\NET_work\\data"
-    get_all_data(['300730.SZ', '002628.SZ', '300144.SZ', '000017.SZ'], path)
+    stock_list = ['300730.SZ', '002628.SZ', '300144.SZ', '000017.SZ']
+
+    get_all_data(stock_list, path)
 
     # '002628'
     # pro = ts.pro_api()
