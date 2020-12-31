18ffc04a5a3d301a61cc432f244332c28726f686
xiejunbiao <996790454@qq.com>
2020-12-31 08:50:02 +0800

    [FIX]

--- scikit_opt/func.py
+++ scikit_opt/func.py
@@ -55,6 +55,9 @@ class FuncSet(object):
             c = arr[i:i + size]
             s.append(c)
         return s
+    # Attribute
+    # reduction and optimization
+    # algorithm
 
 
 if __name__ == '__main__':
